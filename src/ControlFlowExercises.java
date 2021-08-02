7fd81565a6850b5fccfb8a44156317b6c957b0d9
Cierra Smith <cierra.r.smith3@gmail.com>
2021-08-02 16:55:15 -0500

    completed loop basics exercise portion

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -9,12 +9,45 @@ public class ControlFlowExercises {
 //        }
 
         // b. Do While
-        int i = 0;
-        do {
-            System.out.println(i);
-            i+=2;
-        }
-        while (i <= 100);
+        // Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i+=2;
+//        }
+//        while (i <= 100);
+
+        // Alter your loop to count backwards by 5's from 100 to -10.
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i-=5;
+//        }
+//        while (i >= -10);
+
+        // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000.
+//        long i = 2;
+//
+//        do {
+//            System.out.println(i);
+//            i*=i;
+//        }
+//        while (i < 1000000);
+
+        //c. For
+        // Refactor the previous two exercises to use a for loop instead.
+
+//        for (int i = 5; i <= 15 ; i++) {
+//            System.out.println(i);
+//        }
+
+//        for (int i = 0; i <= 100 ; i+=2) {
+//            System.out.println(i);
+//        }
+
+//        for (long i = 2; i < 1000000 ; i*=i) {
+//            System.out.println(i);
+//        }
 
     }
 }
