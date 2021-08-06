b8297bd297c0b8a18274bb199d4ffae5ec16d8f1
Cierra Smith <cierra.r.smith3@gmail.com>
2021-08-06 16:13:35 -0500

    updated the side portion, completed shapes

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,19 +1,21 @@
 package shapes;
 
 public class Square extends Rectangle {
+
+    public int side;
+
     public Square(int side) {
         super(side, side);
+        this.side = side;
     }
 
     @Override
     public double getArea() {
-        double side = super.getWidth();
         return 4 * side;
     }
 
     @Override
     public double getPerimeter() {
-        double side = super.getLength();
         return side * side;
     }
 }
