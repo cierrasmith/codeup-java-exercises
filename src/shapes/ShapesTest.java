92bd57ccd6e22be95770c3a0ede3b18b455ebfc8
Cierra Smith <cierra.r.smith3@gmail.com>
2021-08-09 16:52:36 -0500

    updated shapesTest portion

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,13 +3,14 @@ package shapes;
 public class ShapesTest {
 
     public static void main(String[] args) {
-        Measurable myShape = new Rectangle(4, 5);
-        System.out.println(myShape.getPerimeter());
-        System.out.println(myShape.getArea());
+        Measurable myShape;
+        myShape = new Rectangle(4, 5);
+        System.out.println("The perimeter of the rectangle is: " + myShape.getPerimeter());
+        System.out.println("The area of the rectangle is: " + myShape.getArea());
 
-        Measurable myShape2 = new Square(5);
-        System.out.println(myShape2.getPerimeter());
-        System.out.println(myShape2.getArea());
+        myShape = new Square(5);
+        System.out.println("The perimeter of the square: " + myShape.getPerimeter());
+        System.out.println("The area of the square: " + myShape.getArea());
 
 //        Rectangle box1 = new Rectangle(4, 5);
 //        System.out.println(box1.getPerimeter());
